ac53e2905c392c8f6b247c7071a0ec740d279332
tauraamui <adampstringer@protonmail.com>
2024-12-02 12:06:42 +0000

    feat: generate new temp dir on start

--- src/main.v
+++ src/main.v
@@ -1,5 +1,16 @@
 module main
 
+import io.util
+import os
+
+fn create_tmp_home() !string {
+	mh_root := util.temp_dir() or { return error("unable to create tmp home parent directory: ${err}") }
+	home_dir_path := os.join_path(mh_root, "home")
+	os.mkdir(home_dir_path) or { return error("unable to create tmp home dir ${home_dir_path}: ${err}") }
+	return home_dir_path
+}
+
 fn main() {
-	println('Hello World!')
+	tmp_home := create_tmp_home()!
+	defer { os.rmdir(tmp_home) or { panic(err) } }
 }
